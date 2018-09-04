4f4767e331516d0deb6503a16c90b0c031c60dc4
DateBro <sdudatebro@gmail.com>
2018-09-05 03:38:00 +0800

    稍微完善了store界面的交互，只是因为没有当日交易额的接口先搁置。

--- app/src/main/java/com/example/thinkpad/libra/home/store/StoreFragment.kt
+++ app/src/main/java/com/example/thinkpad/libra/home/store/StoreFragment.kt
@@ -4,12 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.thinkpad.libra.R
 import com.example.thinkpad.libra.login.LoginActivity
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog
 import kotlinx.android.synthetic.main.store_fragment.*
 
 class StoreFragment: Fragment() {
@@ -30,7 +30,32 @@ class StoreFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        exit_text.setOnClickListener { exitHome() }
+        initView()
+    }
+
+    private fun initView() {
+        exit_linearLayout.setOnClickListener { exitHome() }
+        about_linearLayout.setOnClickListener { showAboutUsInfoDialog() }
+        setting_linearLayout.setOnClickListener { showSettingInfoDialog() }
+    }
+
+    private fun showAboutUsInfoDialog() {
+        SweetAlertDialog(activity, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+                .setTitleText("关于我们")
+                .setContentText("SDU OUT 工作室齐鲁软件大赛参赛作品")
+                .setConfirmText("OK")
+                .setCustomImage(R.drawable.sdu)
+                .setConfirmClickListener(null)
+                .show()
+    }
+
+    private fun showSettingInfoDialog() {
+        SweetAlertDialog(activity, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("设置")
+                .setContentText("暂时没有设置相关选项，敬请谅解。")
+                .setConfirmText("OK")
+                .setConfirmClickListener(null)
+                .show()
     }
 
     private fun exitHome() {
